f93608f4a1387b0cc4f047471848af278bb30c42
Alexander Karren <alexanderkarren@Alexanders-MacBook-Pro.local>
2020-04-09 16:50:10 -0600

    addeddded styles to main page

--- api/server.js
+++ api/server.js
@@ -17,7 +17,13 @@ server.use('/api/users', userRouter);
 server.use('/api/posts', postRouter);
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  res.send(`
+  <div style="font-family:'Arial',sans-serif;display:flex;flex-direction:column;align-items:center;">
+    <h1>Frodo and Friends</h1>
+    <h2>IT'S WORKING</h2>
+    <img src="https://media.tenor.com/images/7e567df8c48649cc2d40caadcd127523/tenor.gif" alt="frodooo"/>
+  </div>
+  `);
 });
 
 function logger(req, res, next) {
