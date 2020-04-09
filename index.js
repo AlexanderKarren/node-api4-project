9de260db135ffc479a865a43f02b1db06540ef2f
Alexander Karren <alexanderkarren@Alexanders-MacBook-Pro.local>
2020-04-09 16:36:45 -0600

    moved some stuff around, added dotenv

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config(); // reads values from .env file and merges them into process.env
-const server = require('./server')
 
-const port = 5000;
+const server = require('./api/server')
+
+const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`server listening on port ${port}`))
